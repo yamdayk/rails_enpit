70d24014f1d728ff0f068bd74919a1908662bb87
Yoko Yamada <y_sun_yoko@yahoo.co.jp>
2016-09-22 09:03:45 +0000

    Rollback

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160922064138) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "books", force: :cascade do |t|
-    t.string   "title"
-    t.string   "author"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
 end
